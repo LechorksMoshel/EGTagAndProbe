95f1d4e18e93911dccb22e540b6ea95b4200f76c
Siqi <yuansiqi0114@hotmail.com>
2018-05-29 04:56:21 -0500

    modify Nutuplizer and change db file

--- EGTagAndProbe/plugins/Ntuplizer.cc
+++ EGTagAndProbe/plugins/Ntuplizer.cc
@@ -454,7 +454,7 @@ void Ntuplizer::beginJob()
         this -> _tree -> Branch("UnpackedDR", &_Unpacked_dR, "UnpackedDR/F");
         this -> _tree -> Branch("EmulatedDR", &_Emulated_dR, "EmulatedDR/F");
 
-        this -> _tree -> Branch("seedTowerEt", &_seedTowerEt, "seedTowerEt/I");
+        //this -> _tree -> Branch("seedTowerEt", &_seedTowerEt, "seedTowerEt/I");
    
         this -> _tree -> Branch("TowerHoE", &_TowerHoE,"TowerHoE/I");
 
@@ -819,11 +819,11 @@ void Ntuplizer::analyze(const edm::Event& iEvent, const edm::EventSetup& eSetup)
 						this -> _l1tEmuRawEt     = l1tEmuEG.rawEt();
 						this -> _l1tEmuIsoEt     = l1tEmuEG.isoEt();
                                                 
-                                                this -> _seedTowerEt     = l1tEmuEG.seedTowerEt(); 
+                                                //this -> _seedTowerEt     = l1tEmuEG.seedTowerEt(); 
  
                                                 this -> _TowerHoE        = l1tEmuEG.towerHoE();  
    
-                                                this -> _iem             = l1tEmuEG.seedTowerEm();
+                                                //this -> _iem             = l1tEmuEG.seedTowerEm();
 
                                                 //this -> _ihad            = l1tEmuEG.seedTowerHad();
 
